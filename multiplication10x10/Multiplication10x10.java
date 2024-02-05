3a1f88f72d1c796af1ab0e2cf209de44cd95a7ff
Bryan McKinsley Caranto <brymckinsley@gmail.com>
2024-02-05 23:32:34 +0800

    Added multiplication10x10

---
+++ multiplication10x10/Multiplication10x10.java
@@ -0,0 +1,19 @@
+package multiplication10x10;
+
+public class Multiplication10x10 {
+  
+    public static void main(String[] args) {
+      
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+             
+                int product = i * j;
+               
+                System.out.print(product + "\t");
+            }
+        
+            System.out.println();
+        }
+    }
+}
+
