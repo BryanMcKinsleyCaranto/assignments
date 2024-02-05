90b3fa9a6f04a8505afc17f1a3e8e1a550487dea
Bryan McKinsley Caranto <brymckinsley@gmail.com>
2024-02-05 23:05:49 +0800

    Added largestnumber

---
+++ largestnumber/LargestNumber.java
@@ -0,0 +1,33 @@
+package largestnumber;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter three numbers
+        System.out.println("Enter three numbers:");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+
+        // Find the largest number
+        int largest = num1;
+        if (num2 > largest) {
+            largest = num2;
+        }
+        if (num3 > largest) {
+            largest = num3;
+        }
+
+        // Output the result
+        if (num1 == num2 && num2 == num3) {
+            System.out.println("All numbers are equal");
+        } else {
+            System.out.println("Largest number is: " + largest);
+        }
+
+        scanner.close();
+    }
+}
